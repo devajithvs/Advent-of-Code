910417d7290bbe0e50ccf1bc4a7be3355edce37c
Devajith Valaparambil Sreeramaswamy <valaparambilsreeramaswamydv@cardiff.ac.uk>
2021-12-13 07:00:07 +0000

    Day 13

---
+++ Python/day13/q2.py
@@ -0,0 +1,73 @@
+def print_array(array, x_lim=None, y_lim=None):
+    if y_lim:
+        array = array[:y_lim]
+    for i in array:
+        for j in i[:x_lim]:
+            print(str(j), end =" ")
+        print()
+
+def Solution(file_content):
+    coordinates = []
+    instructions = []
+    for value in file_content:
+        value = value.strip()
+        coordinate = value.split(",")
+        if len(coordinate) >= 2:
+            coordinates.append([int(coordinate[0]), int(coordinate[1])])
+        elif len(coordinate) == 1 and coordinate[0]:
+            instruction = value.split("=")
+            instructions.append([instruction[0][-1], int(instruction[1])])
+    
+    x_pos = []
+    y_pos = []
+    for point in coordinates:
+        x_pos.append(point[0])
+        y_pos.append(point[1])
+    paper = [ [0]*(max(x_pos)+1) for _ in range(max(y_pos)+1)]
+    for point in coordinates:
+        j = point[0]
+        i = point[1]
+        paper[i][j] = 1
+    
+    # print_array(paper)
+
+    for fold in instructions[:]:
+        paper_x = len(paper[0])
+        paper_y = len(paper)
+
+        if fold[0] == "y":
+            len_y = max(paper_y-fold[1], fold[1])-1
+            len_x = paper_x
+            new_paper = [ [0]*len_x for _ in range(len_y)]
+            for ind_i in reversed(range(0,paper_y)):
+                if len_y-ind_i >= 0 and ind_i!=fold[1]:
+                    if (len_y-ind_i)%len_y > 0:
+                            second_half = len_y+(len_y-ind_i)%len_y
+                    else:
+                        second_half = 2*len_y
+                    for ind_j in range(paper_x):
+                        new_paper[ind_i][ind_j] = paper[ind_i][ind_j] + paper[second_half][ind_j]
+            paper = new_paper
+
+        elif fold[0] == "x":
+            len_x = max(paper_x-fold[1], fold[1])-1
+            len_y = paper_y
+            new_paper = [ [0]*len_x for _ in range(len_y)]
+
+            for ind_j in reversed(range(0,paper_x)):
+                if len_x-ind_j >= 0 and ind_j!=fold[1]:
+                    if (len_x-ind_j)%len_x > 0:
+                        second_half = len_x+(len_x-ind_j)%len_x
+                    else:
+                        second_half = 2*len_x
+                    for ind_i in range(paper_y):
+                        # print(ind_j, len_x, paper_x, second_half)
+                        new_paper[ind_i][ind_j] = paper[ind_i][ind_j] + paper[ind_i][second_half]
+            paper = new_paper
+
+    for i in range(len(paper)):
+        for j in range(len(paper[i])):
+            if paper[i][j]:
+                paper[i][j] = "X"
+    
+    print_array(paper)#, x_lim=50, y_lim=20)
